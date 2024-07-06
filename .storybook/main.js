8aefb540d1e2acb158442ecc3b0ddbe7082c89e3
Pierre Bourgeois <pbourgeois@soquij.qc.ca>
2024-07-06 06:12:04 -0400

    maj storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,20 @@ const config = {
     "../components/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../components/**/*.mdx",
   ],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
   ],
+
   framework: {
     name: "@storybook/html-vite",
     options: {},
   },
+
+  docs: {}
 };
 export default config;
